e84f1b8b951bb1e85cb38ce5c4aae8734d6ed7de
dewyeon <juyon98@korea.ac.kr>
2022-03-01 22:54:45 +0900

    fix: gpu num error

--- realnvp.py
+++ realnvp.py
@@ -7,9 +7,9 @@ n = args.n
 
 if args.n == 1:
     for dataset in ['checkerboard', '2spirals', 'moons', 'pinwheel', '8gaussians']:
-        subprocess.call(f"python realnvp_toy_experiment.py --no_tensorboard --dataset {dataset} --num_flows 4 --learning_rate 1e-4 --experiment_name {'k4_realnvp'+str(dataset)} --gpu {n}", shell=True)
+        subprocess.call(f"python realnvp_toy_experiment.py --no_tensorboard --dataset {dataset} --num_flows 4 --learning_rate 1e-4 --experiment_name {'k4_realnvp'+str(dataset)} --gpu_id {n}", shell=True)
 
 if args.n == 2:
     for dataset in ['checkerboard', '2spirals', 'moons', 'pinwheel', '8gaussians']:
-        subprocess.call(f"python realnvp_toy_experiment.py --no_tensorboard --dataset {dataset} --num_flows 8 --learning_rate 1e-4 --experiment_name {'k8_realnvp'+str(dataset)} --gpu {n}", shell=True)
+        subprocess.call(f"python realnvp_toy_experiment.py --no_tensorboard --dataset {dataset} --num_flows 8 --learning_rate 1e-4 --experiment_name {'k8_realnvp'+str(dataset)} --gpu_id {n}", shell=True)
 
